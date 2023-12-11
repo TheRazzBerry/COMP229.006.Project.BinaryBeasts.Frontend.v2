2eedc21f23997b98e3d4a4c9de0167f38cb79845
TheRazzBerry <razeen.hrahman@gmail.com>
2023-12-11 06:21:40 -0500

    Added Sign Out

--- src/components/nav/navButton.jsx
+++ src/components/nav/navButton.jsx
@@ -1,16 +1,23 @@
 // Import Components
 import AuthModal from "../auth/authModal";
-import { isAuthenticated } from "../auth/authHelper";
+import { isAuthenticated, clearJWT } from "../auth/authHelper";
 
 export default function NavButton() {
     function NavToggler(props) {
+
+        function signOut() {
+            clearJWT();
+            window.location.reload(false);
+        }
+
         return (<>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu" aria-controls="navMenu" aria-expanded="false" aria-label="Toggle Navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navMenu">
-                <div className="navbar-nav">
+                <div className="navbar-nav ms-auto">
                     <a className="nav-link" href="users">My Tournaments</a>
+                    <button className="nav-link text-start" onClick={signOut}>Sign Out</button>
                 </div>
             </div>
         </>);
